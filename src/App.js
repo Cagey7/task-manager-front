a7d9b23e073091b307b8da282e748556c48c557d
Cagey7 <dagodecagey@gmail.com>
2024-12-16 23:14:08 +0500

    added authcontext

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import { RouterProvider } from 'react-router-dom';
-import { createBrowserRouter } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage'
-import LoginPage from './pages/LoginPage/LoginPage'
-import RegisterPage from './pages/RegisterPage/RegisterPage'
-import Layout from './components/Layout/Layout';
-import './App.css';
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      { path: "/", element: <HomePage /> },
-      { path: "/login", element: <LoginPage /> },
-      { path: "/register", element: <RegisterPage /> },
-    ],
-  },
-]);
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import Layout from "./components/Layout/Layout";
+import AuthProvider from "./contexts/AuthContext";
 
 function App() {
   return (
-    <RouterProvider router={router} />
+    <div className="App">
+      <Router>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<HomePage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+            </Route>
+          </Routes>
+        </AuthProvider>
+      </Router>
+    </div>
   );
 }
 
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,6 +1,10 @@
+import { useAuth } from '../../contexts/AuthContext'
+
 function HomePage() {
+    const user = useAuth();
     return (
         <>
+            <p>{user.user}</p>
             <h2>Главная</h2>
         </>
     )
