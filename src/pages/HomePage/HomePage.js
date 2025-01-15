bf0749de0b73b3a4a0d6984e86027400d7e41f4c
Cagey7 <dagodecagey@gmail.com>
2025-01-15 22:39:15 +0500

    HomePage add task backend

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,15 +1,58 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AuthProvider from "../../contexts/AuthContext";
 import NewTask from "../../components/NewTask/NewTask";
 import Task from "../../components/Task/Task";
-import "./HomePage.css"
-
+import "./HomePage.css";
 
 function HomePage() {
   const [tasks, setTasks] = useState([]);
   const [CompletedTask, setCompletedTask] = useState([]);
 
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/tasks/", {
+      headers: {
+        Authorization: `Token ${localStorage.getItem("token")}`,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        const newTasks = data.map((task) => ({
+          task_header: task.title,
+          task_desk: task.desc,
+        }));
+        setTasks(newTasks);
+      })
+      .catch((error) => console.error("Ошибка при запросе:", error));
+  }, []);
+
   function addTask(task_header, task_desk) {
+    fetch("http://127.0.0.1:8000/create-task/", {
+      method: "POST",
+      headers: {
+        Authorization: `Token ${localStorage.getItem("token")}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: task_header,
+        desc: task_desk
+      }),
+    })
+    .then((response) => {
+      console.log(response)
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .catch((error) => console.error("Ошибка при запросе:", error));
+
     setTasks([...tasks, { task_header, task_desk }]);
   }
 
@@ -29,29 +72,29 @@ function HomePage() {
         <NewTask addTask={addTask} />
         <div className="user-task-list">
           {tasks.map((task, index) => (
-              <Task
+            <Task
+              key={`task-${index}`}
               taskId={index}
               taskHeader={task.task_header}
               taskDesk={task.task_desk}
               removeTask={removeTask}
               addCompletedTask={addCompletedTask}
               taskStatus="in_process"
-              />
+            />
           ))}
-      </div>
+        </div>
         <h3 className="complete-task-header">Выполненные задания</h3>
         <div className="user-task-list">
           {CompletedTask.map((task, index) => (
-              <Task
+            <Task
               taskId={index}
               taskHeader={task.task_header}
               taskDesk={task.task_desk}
               taskStatus="completed"
-              />
+            />
           ))}
         </div>
-
-    </>
+      </>
     </AuthProvider>
   );
 }
